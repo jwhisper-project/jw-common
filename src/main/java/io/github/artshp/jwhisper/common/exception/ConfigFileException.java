c61e0a5d6bee3e6aeadaf53306717b4c79675f91
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-03 17:25:34 +0200

    Add common exceptions

---
+++ src/main/java/io/github/artshp/jwhisper/common/exception/ConfigFileException.java
@@ -0,0 +1,10 @@
+package io.github.artshp.jwhisper.common.exception;
+
+import lombok.experimental.StandardException;
+
+/**
+ * Exception for issues with config file.
+ */
+@StandardException
+public class ConfigFileException extends RuntimeException {
+}
---
+++ src/main/java/io/github/artshp/jwhisper/common/exception/WrongPasswordException.java
@@ -0,0 +1,10 @@
+package io.github.artshp.jwhisper.common.exception;
+
+import lombok.experimental.StandardException;
+
+/**
+ * Exception for wrong provided password.
+ */
+@StandardException
+public class WrongPasswordException extends RuntimeException {
+}
