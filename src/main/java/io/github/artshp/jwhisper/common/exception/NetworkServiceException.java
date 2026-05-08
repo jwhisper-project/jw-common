e1e4a672ff8af602ff5f3e849a61307567c2ea58
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-08 12:34:54 +0200

    Add network service exception

---
+++ src/main/java/io/github/artshp/jwhisper/common/exception/NetworkServiceException.java
@@ -0,0 +1,10 @@
+package io.github.artshp.jwhisper.common.exception;
+
+import lombok.experimental.StandardException;
+
+/**
+ * Exception when network server fails to process request.
+ */
+@StandardException
+public class NetworkServiceException extends Exception {
+}
