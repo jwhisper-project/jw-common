106a59bca0ead9cad598708bfb669f23f690d2b6
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-08 11:43:00 +0200

    Update signing utils

--- src/main/java/io/github/artshp/jwhisper/common/crypto/SigningUtils.java
+++ src/main/java/io/github/artshp/jwhisper/common/crypto/SigningUtils.java
@@ -1,26 +1,36 @@
 package io.github.artshp.jwhisper.common.crypto;
 
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
+import lombok.extern.slf4j.Slf4j;
 
+import java.security.*;
+
+@Slf4j
 public class SigningUtils {
 
-    public static byte[] sign(PrivateKey privateKey, byte[] data) throws Exception {
+    public static byte[] sign(PrivateKey privateKey, byte[] data) {
         Signature sig = SecurityUtils.newSignature();
 
-        sig.initSign(privateKey);
-        sig.update(data);
+        try {
+            sig.initSign(privateKey);
+            sig.update(data);
 
-        return sig.sign();
+            return sig.sign();
+        } catch (InvalidKeyException | SignatureException e) {
+            throw new IllegalStateException("Unexpected error during signing data.", e);
+        }
     }
 
-    public static boolean verify(PublicKey publicKey, byte[] data, byte[] signature) throws Exception {
+    public static boolean verify(PublicKey publicKey, byte[] data, byte[] signature) {
         Signature sig = SecurityUtils.newSignature();
 
-        sig.initVerify(publicKey);
-        sig.update(data);
+        try {
+            sig.initVerify(publicKey);
+            sig.update(data);
 
-        return sig.verify(signature);
+            return sig.verify(signature);
+        } catch (InvalidKeyException | SignatureException e) {
+            log.warn("Unexpected error during verifying signature.", e);
+            return false;
+        }
     }
 }
