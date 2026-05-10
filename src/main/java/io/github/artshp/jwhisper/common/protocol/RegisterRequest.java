7aa6ea7fcdf3b55f095bcdc461b8164c3f525c5b
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-10 18:39:26 +0200

    Update register request

--- src/main/java/io/github/artshp/jwhisper/common/protocol/RegisterRequest.java
+++ src/main/java/io/github/artshp/jwhisper/common/protocol/RegisterRequest.java
@@ -2,7 +2,8 @@ package io.github.artshp.jwhisper.common.protocol;
 
 public record RegisterRequest(
         String username,
-        byte[] publicKey,
-        byte[] usernameSignature
+        byte[] publicSigningKey,    // Ed25519
+        byte[] publicEncryptionKey, // X25519
+        byte[] ownershipSignature   // Sender signs his username with the signing key
 ) implements WhisperMessage {
 }
