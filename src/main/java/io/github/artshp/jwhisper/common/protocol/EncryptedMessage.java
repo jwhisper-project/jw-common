eaaa183d3725026f1197664d5d85cd802fdf9d02
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-10 18:22:30 +0200

    Update encrypted message

--- src/main/java/io/github/artshp/jwhisper/common/protocol/EncryptedMessage.java
+++ src/main/java/io/github/artshp/jwhisper/common/protocol/EncryptedMessage.java
@@ -3,8 +3,10 @@ package io.github.artshp.jwhisper.common.protocol;
 public record EncryptedMessage(
         String sender,
         String recipient,
-        byte[] message,
-        byte[] signature,
+        byte[] ephemeralPublicKey, // sender's one-time X25519 key
+        byte[] nonce,              // AES-GCM IV
+        byte[] message,            // the encrypted message
+        byte[] signature,          // signed sender's Ed25519 key
         long timestamp
 ) implements WhisperMessage {
 }
