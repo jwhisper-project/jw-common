750e3f6227a8b81107fc61647b1296db27d0cd11
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-10 18:42:52 +0200

    Update user public key response

--- src/main/java/io/github/artshp/jwhisper/common/protocol/UserPublicKeyResponse.java
+++ src/main/java/io/github/artshp/jwhisper/common/protocol/UserPublicKeyResponse.java
@@ -2,7 +2,8 @@ package io.github.artshp.jwhisper.common.protocol;
 
 public record UserPublicKeyResponse(
         String targetUsername,
-        byte[] targetPublicKey,
+        byte[] publicSigningKey,    // Ed25519
+        byte[] publicEncryptionKey, // X25519
         boolean found
 ) implements WhisperMessage {
 }
