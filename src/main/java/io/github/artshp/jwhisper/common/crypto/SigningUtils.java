463b9f952d36a74e6773bea569748a958bd0c21f
Artemiy Shipovalov <56469391+ArtShp@users.noreply.github.com>
2026-05-16 21:50:11 +0200

    Add SigningUtils javadoc

--- src/main/java/io/github/artshp/jwhisper/common/crypto/SigningUtils.java
+++ src/main/java/io/github/artshp/jwhisper/common/crypto/SigningUtils.java
@@ -4,9 +4,22 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.security.*;
 
+/**
+ * Signing utils.
+ */
 @Slf4j
-public class SigningUtils {
+public final class SigningUtils {
 
+    private SigningUtils() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    /**
+     * Sign data using private key.
+     * @param privateKey private key
+     * @param data data to sign
+     * @return data signature
+     */
     public static byte[] sign(PrivateKey privateKey, byte[] data) {
         Signature sig = SecurityUtils.newSignature();
 
@@ -20,6 +33,13 @@ public class SigningUtils {
         }
     }
 
+    /**
+     * Verify data signature using public key.
+     * @param publicKey public key
+     * @param data data to verify
+     * @param signature data signature
+     * @return {@code true} if signature is valid, otherwise {@code false}
+     */
     public static boolean verify(PublicKey publicKey, byte[] data, byte[] signature) {
         Signature sig = SecurityUtils.newSignature();
 
